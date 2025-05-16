e0c5905aa46895c74c76ae33c8280b72555ad4a9
LarryKithinji <LawrenceMwenda1@outlook.com>
2025-05-16 14:33:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,185 +1,263 @@
 import praw
-import requests
-from newspaper import Article
-import textwrap
+import smrzr
+from altsummary import summary
 import os
-import time
-import argparse
-from dotenv import load_dotenv
-import logging
-import traceback
-from transformers import pipeline
+import find_other_news_sources
+import itertools
+from prawoauth2 import PrawOAuth2Mini
+import blacklist
+from time import sleep
+import subreddits
+from newspaper import Article
+import message_templates as mt
 
-# --- Logging Setup ---
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    handlers=[logging.FileHandler("summarizer.log"), logging.StreamHandler()]
-)
-logger = logging.getLogger("Reddit News Summarizer")
 
-# --- Load environment variables ---
-load_dotenv()
+def init():
+    r = praw.Reddit(user_agent="Samachar Bot for /r/india by /u/sallurocks")
+    scopes = {u'edit', u'submit', u'read', u'privatemessages', u'identity', u'history'}
+    oauth_helper = PrawOAuth2Mini(r, app_key=os.environ['app_key'],
+                                  app_secret=os.environ['app_secret'],
+                                  access_token=os.environ['access_token'],
+                                  refresh_token=os.environ['refresh_token'], scopes=scopes)
+    subreddit = r.get_subreddit("+".join(subreddits.subreddits))
+    return r, oauth_helper, subreddit
 
-# --- Global summarizer model ---
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
-# --- Reddit client setup ---
-def setup_reddit_client():
+def postTracker():
+    """File to keep track of posts already looked, gets refreshed if bot is restarted.
+    put pointer at start, then reads the file to string and then puts the pointer back at the end."""
+
+    fo = open("looked.txt", "a+")
+    position = fo.tell()
+    fo.seek(0, 0)
+    str1 = fo.read()
+    fo.seek(position)
+    return fo, str1
+
+
+def addSubmission(file_handler, submission):
+    file_handler.write(submission.id + " ")
+
+
+def isPosted(comments):
+    for comment in comments:
+        if str(comment.author) == 'samacharbot2':
+            return True
+        else:
+            return False
+
+
+def summarizeSMRZR(url):
+    """get post information and summary using Smrzr. Format properly."""
+
+    summ_article = smrzr.Summarizer(url, 4, 'default', 'newspaper')
+    keypoints = summ_article.keypoints
+    summ = summ_article.summary
+    message = "\n\n> * ".join(keypoints)
+    message = "> * " + message
+    message = message.replace("`", "")
+    message = message.replace("#", "\#")
+    return summ, message, summ_article.text
+
+
+def processSummarization(file_handler, submission, method):
+    if method == 'smrzr':
+        addSubmission(file_handler, submission)
+
+    if isPosted(submission.comments):
+        print "already posted"
+        return
+
+    if method == 'smrzr':
+        summ, keypoints, article_text = summarizeSMRZR(submission.url)
+        print "Finished " + str(submission.id) + " with smrzr"
+    elif method == 'other':
+        summ, keypoints, article_text = summarizeOther(submission.url)
+        print "Finished " + str(submission.id) + " with other"
+    else:
+        return
+
+    if len(keypoints) < 150:
+        return
+
+    relevant_message = getRelevant(submission.url)
+
+    if finalChecks(article_text):
+        new_comment = summ + mt.br + keypoints.encode('ascii', 'ignore') + mt.br + relevant_message + mt.br + mt.endmsg
+        postComment(submission, new_comment)
+
+
+def summarizeOther(url):
+    article = Article(url)
+    article.download()
+    article.parse()
+    text = article.text
+    keypoints = summary(text)
+    summ = article.title
+    keypoints = keypoints.replace("`", "")
+    keypoints = keypoints.replace("#", "\#")
+
+    return summ, keypoints, text
+
+
+def finalChecks(text):
+    if text == 'Never miss a great news story!\n\nGet instant notifications from Economic Times\n\nAllowNot ' \
+               'now\n\nYou can switch off notifications anytime using browser settings.':
+        print "check failed"
+        return False
+    else:
+        return True
+
+
+def postComment(submission, comment):
     try:
-        reddit = praw.Reddit(
-            client_id=os.getenv("REDDIT_CLIENT_ID"),
-            client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
-            username=os.getenv("REDDIT_USERNAME"),
-            password=os.getenv("REDDIT_PASSWORD"),
-            user_agent=os.getenv("REDDIT_USER_AGENT", "NewsArticleSummarizer v1.0")
-        )
-        logger.info(f"Authenticated as {reddit.user.me()}")
-        return reddit
+        submission.add_comment(comment)
+
     except Exception as e:
-        logger.error(f"Failed to authenticate with Reddit:\n{traceback.format_exc()}")
-        raise
+        print "Error posting comment\n"
+        print type(e)
+        print e.args
+        print e
+        print submission.id
+        print "\n"
 
-# --- Article extraction ---
-def extract_article_content(url):
-    try:
-        article = Article(url)
-        article.download()
-        article.parse()
 
-        if len(article.text) < 100:
-            logger.warning(f"Article too short ({len(article.text)} characters). Might be poorly extracted.")
-        
-        return {
-            "title": article.title,
-            "text": article.text,
-            "authors": article.authors,
-            "publish_date": article.publish_date,
-            "top_image": article.top_image
-        }
-    except Exception as e:
-        logger.error(f"Error extracting article from {url}:\n{traceback.format_exc()}")
-        return None
+def getRelevant(link):
+    relevant_list = find_other_news_sources.find_other_news_sources(url=link)
+    if relevant_list is None or len(relevant_list) == 0:
+        relevant_message = mt.relevant_message
+    else:
+        relevant_title = []
+        relevant_link = []
+        relevant_message = mt.relevant_message
+        # temp variable to see if new link isn't same as last one.
+        temp_blink = ""
+        for a_link in relevant_list:
+            relevant_title.append(a_link[0])
+            relevant_link.append(a_link[1])
 
-# --- Summarization ---
-def summarize_text(text, max_length=150, min_length=40):
-    try:
-        max_chunk_size = 1024
-        chunks = textwrap.wrap(text, max_chunk_size)
-        summaries = []
+        for (a_link, b_link) in itertools.izip(relevant_title, relevant_link):
 
-        for chunk in chunks:
-            if len(chunk) < 50:
+            # if no title found, name the link.
+            if a_link is None:
+                a_link = "This"
+            if b_link is None or b_link == link:
                 continue
-            result = summarizer(chunk, max_length=max_length, min_length=min_length, do_sample=False)
-            if result:
-                summaries.append(result[0]['summary_text'])
+            if temp_blink == b_link:
+                continue
+            try:
+                relevant_message = relevant_message + "\n\n" + "> * " + "[" + a_link + "]" + "(" + b_link + ")"
+                temp_blink = b_link
+            except Exception as e:
+                print e
 
-        final_summary = " ".join(summaries)
-        if len(final_summary) > 9500:
-            final_summary = final_summary[:9500] + "..."
+    return relevant_message
 
-        return final_summary
-    except Exception as e:
-        logger.error(f"Failed to summarize text:\n{traceback.format_exc()}")
-        return "Error generating summary."
 
-# --- Comment formatting ---
-def format_reddit_comment(article_data, summary):
-    comment = f"# {article_data['title']}\n\n"
+def checkDelete(r):
+    unread = r.get_unread(limit=None)
+    for msg in unread:
 
-    if article_data.get('authors'):
-        comment += f"**Authors:** {', '.join(article_data['authors'])}\n\n"
+        # only works if the word delete is posted as it is, without edit.
+        if msg.body.lower() == 'delete':
+            try:
 
-    if article_data.get('publish_date'):
+                # get comment id from message.
+                idd = msg.id
+                idd = 't1_' + idd
+
+                # find comment from id
+                comment = r.get_info(thing_id=idd)
+
+                # find parent comment i.e samachar bot comment
+                parentid = comment.parent_id
+                comment_parent = r.get_info(thing_id=parentid)
+
+                # get submission author through submission link id
+                sublink = comment_parent.link_id
+                author1 = r.get_info(thing_id=sublink)
+
+                # verify author of message is OP, then delete message and mark unread.
+                if str(msg.author.name) == str(author1.author):
+                    comment_parent.delete()
+                    msg.mark_as_read()
+                else:
+                    msg.mark_as_read()
+
+            except Exception as e:
+                print "Error in delete"
+                print type(e)
+                print e.args
+                print e
+                print "\n"
+                msg.mark_as_read()
+                continue
+
+
+def checkConditions(submission, ids):
+    return str(submission.domain) != str("self." + str(submission.subreddit)) \
+           and submission.domain not in blacklist.blocked and submission.id not in ids
+
+
+def start():
+
+    r, oauth_helper, subreddit = init()
+
+    while True:
+
         try:
-            comment += f"**Published:** {article_data['publish_date'].strftime('%Y-%m-%d')}\n\n"
-        except Exception:
-            pass
-
-    comment += "## Summary\n\n"
-    comment += summary + "\n\n"
-    comment += "---\n"
-    comment += "*This summary was automatically generated. I am a bot.*"
-
-    return comment
-
-# --- Process submissions ---
-def process_new_submissions(subreddit_name, processed_ids_file="processed_ids.txt"):
-    processed_ids = set()
-    if os.path.exists(processed_ids_file):
-        with open(processed_ids_file, "r") as f:
-            processed_ids = set(line.strip() for line in f)
-
-    reddit = setup_reddit_client()
-    subreddit = reddit.subreddit(subreddit_name)
-
-    logger.info(f"Monitoring r/{subreddit_name} for new submissions...")
-
-    for submission in subreddit.stream.submissions(skip_existing=True):
-        if submission.id in processed_ids or submission.is_self:
+            submissions = subreddit.get_new(limit=50)
+        except praw.errors.HTTPException:
+            print "HTTP Exception"
+            sleep(300)
             continue
 
-        url = submission.url
-        if any(ext in url for ext in ['.com/', '.org/', '.net/', '.gov/', '.edu/']):
-            logger.info(f"Processing: {submission.title} - {url}")
-            article_data = extract_article_content(url)
+        nothing = True
+        for submission in submissions:
 
-            if not article_data or not article_data['text']:
-                logger.warning(f"Failed to extract content from {url}")
-                continue
+            print "Working on - " + str(submission.id),
+            fo, ids = postTracker()
+            oauth_helper.refresh()
 
-            summary = summarize_text(article_data['text'])
-            comment_text = format_reddit_comment(article_data, summary)
+            if int(submission.score) >= 0:
 
-            try:
-                submission.reply(comment_text)
-                logger.info(f"Comment posted to: {submission.title}")
-                time.sleep(5)
-            except Exception as e:
-                logger.error(f"Error posting comment:\n{traceback.format_exc()}")
-
-        processed_ids.add(submission.id)
-        with open(processed_ids_file, "a") as f:
-            f.write(f"{submission.id}\n")
-
-# --- Main function ---
-def main():
-    parser = argparse.ArgumentParser(description='Reddit News Article Summarizer')
-    parser.add_argument('--subreddit', '-s', type=str, required=True, help='Subreddit name (without the r/)')
-    parser.add_argument('--monitor', '-m', action='store_true', help='Continuously monitor subreddit')
-
-    args = parser.parse_args()
-
-    if args.monitor:
-        logger.info(f"Continuous monitoring mode for r/{args.subreddit}")
-        try:
-            process_new_submissions(args.subreddit)
-        except KeyboardInterrupt:
-            logger.info("Keyboard interrupt received. Exiting...")
-        except Exception as e:
-            logger.error(f"Monitoring failed:\n{traceback.format_exc()}")
-    else:
-        logger.info(f"One-time check for r/{args.subreddit}")
-        reddit = setup_reddit_client()
-        subreddit = reddit.subreddit(args.subreddit)
-        latest = list(subreddit.new(limit=1))
-
-        if latest:
-            submission = latest[0]
-            if not submission.is_self and any(ext in submission.url for ext in ['.com/', '.org/', '.net/', '.gov/', '.edu/']):
-                logger.info(f"Processing: {submission.title} - {submission.url}")
-                article_data = extract_article_content(submission.url)
-                if article_data and article_data['text']:
-                    summary = summarize_text(article_data['text'])
-                    comment_text = format_reddit_comment(article_data, summary)
+                if checkConditions(submission, ids):
+                    nothing = False
                     try:
-                        submission.reply(comment_text)
-                        logger.info(f"Comment posted to: {submission.title}")
-                    except Exception as e:
-                        logger.error(f"Failed to post comment:\n{traceback.format_exc()}")
-        logger.info("Finished one-time run.")
 
-# --- Run script ---
-if __name__ == "__main__":
-    main()
+                        processSummarization(fo, submission, 'smrzr')
+
+                    except smrzr.ArticleExtractionFail:
+                        print "Article Extraction Failed"
+                        continue
+
+                    # in case Smrzr fails to get summary, try another method
+                    except AssertionError:
+                        print "Assertion Error"
+                        processSummarization(fo, submission, 'other')
+
+                    except Exception as e:
+                        print "Unknown ERROR"
+                        print type(e)
+                        print e.args
+                        print e
+                        print submission.id
+                        print "\n"
+                        continue
+
+                    fo.close()
+
+                else:
+                    print "Nothing to do, checking messages"
+                    # if no more posts to summarize, go through unread messages and see if any posts to delete.
+                    checkDelete(r)
+                    continue
+
+        if nothing:
+            print "Nothing to do, sleeping for 1 minute"
+            sleep(60)
+
+
+
+if __name__ == '__main__':
+    start()
